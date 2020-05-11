25b5659d86c89a0d4334a2dfa7faff1161457c3c
Yuta_Ogawa <oytkt0510@gmail.com>
2020-05-11 13:03:43 +0900

    言語化

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const words = ['apple', 'sky', 'blue'];
-let word = words[Math.floor(Math.random() * words.length)];
+let words= ['apple', 'sky', 'blue'];
+let word;
 let loc;
 let score;
 let miss;
-let timeLimit =  3 * 1000;
+let timeLimit = 1000 * 3;
 let startTime;
 let isPlaying = false;
 
@@ -16,57 +16,37 @@ const timerLabel = document.getElementById('timer');
 
 
 function updateTarget() {
-let placeholder = '';
-for(let i = 0; i < loc; i++) {
-  placeholder += '_';
-}
-target.textContent = placeholder + word.substring(loc);
+ let placeholder = '';
+ for(let i = 0; i< loc; i++){
+   placeholder+='_';
+  }
+  target.textContent = placeholder + word.substring(loc);
 }
 
 function updateTimer() {
-  const timeLeft = startTime + timeLimit -Date.now();
-  timerLabel.textContent = (timeLeft / 1000).toFixed(2);
-
+  let timeLeft = startTime + timeLimit - Date.now();
+  timerLabel.textContent = (timeLeft/1000).toFixed(2);
   const timeOutId = setTimeout(() => {
-updateTimer();
+    updateTimer();
   }, 10);
-
-  if(timeLeft < 0) {
-    isPlaying = false;
+  if(timeLeft< 0) {
     clearTimeout(timeOutId);
     timerLabel.textContent = '0.00';
     setTimeout(() => {
       showResult();
     }, 100);
-
+    isPlaying = false;
     target.textContent = 'click to replay';
   }
 }
 
-function showResult(){
- const accuracy = score + miss === 0 ? 0 : score / (score * miss) * 100
- alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy!`);
+function showResult() {
+ let accuracy = score + miss === 0 ? 0 : score/ (score + miss) * 100;
+ alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy`);
 }
 
-window.addEventListener('click', () => {
-  if(isPlaying === true){
-    return;
-  }
-  isPlaying = true;
-
-  loc = 0;
-  score = 0;
-   miss = 0;
-   scoreLabel.textContent = score;
-   missLabel.textContent = miss;
-   word = words[Math.floor(Math.random() * words.length)];
-  target.textContent = word;
-  startTime = Date.now();
-  updateTimer();
-});
-
-window.addEventListener('keydown', (e) => {
-  if ( isPlaying !== true) {
+window.addEventListener('keydown', (e) =>{
+  if(isPlaying === false) {
     return;
   }
 if(e.key === word[loc]) {
@@ -78,8 +58,271 @@ if(e.key === word[loc]) {
   updateTarget();
   score++;
   scoreLabel.textContent = score;
-} else {
-  miss++;
-  missLabel.textContent = miss;
+}else {
+miss++;
+missLabel.textContent = miss;
 }
 });
+
+window.addEventListener('click', () => {
+ if (isPlaying === true) {
+   return;
+ }
+ score = 0;
+ miss = 0;
+ loc = 0;
+ scoreLabel.textContent = score;
+ missLabel.textContent = miss;
+ word = words[Math.floor(Math.random() * words.length)];
+  isPlaying = true;
+  target.textContent= word;
+  startTime = Date.now();
+  updateTimer();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const words = [
+//   'apple',
+//   'sky',
+//   'middle'
+// ];
+
+// let word;
+// let loc;
+// let score;
+// let miss;
+// let timeLimit = 3 * 1000;
+// let startTime;
+// let isPlaying = false;
+
+// const target = document.getElementById('target');
+// const scoreLabel = document.getElementById('score');
+// const missLabel = document.getElementById('miss');
+// const timerLabel = document.getElementById('timer');
+
+
+// function updateTarget() {
+//   let placeholder = '';
+//   for(let i = 0; i < loc; i++) {
+// placeholder += '_';
+//   }
+//   target.textContent = placeholder + word.substring(loc);
+// }
+
+// function updateTimer() {
+
+//   let timeLeft = startTime + timeLimit - Date.now();
+//   timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+//   let timeOutId = setTimeout(() => {
+// updateTimer();
+//   }, 10);
+
+//   if(timeLeft < 0) {
+//     isPlaying = false;
+//     clearTimeout(timeOutId);
+//     timerLabel.textContent = '0.00';
+//     setTimeout(() => {
+//   showResult();
+//     },100);
+//     target.textContent='click to replay';
+//   }
+// }
+//   function showResult() {
+//   let accuracy = score + miss === 0 ? 0 : score/(score + miss) * 100;
+//   alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy`);
+// }
+
+// window.addEventListener('click', () => {
+//   if(isPlaying === true) {
+//     return;
+//   }
+//   loc = 0;
+//   score = 0;
+//   miss = 0;
+//   scoreLabel.textContent = score;
+//   missLabel.textContent = miss;
+//   word = words[Math.floor(Math.random() * words.length)];
+//   isPlaying = true;
+//   target.textContent = word;
+//   startTime = Date.now();
+//   updateTimer();
+// })
+
+// window.addEventListener('keydown', e => {
+//   if(isPlaying === false) {
+//     return;
+//   }
+// if(word[loc] ===e.key) {
+//   loc++;
+//   score++;
+//   scoreLabel.textContent = score;
+//   if(loc === word.length) {
+//    word = words[Math.floor(Math.random() * words.length)];
+//     loc = 0;
+//   }
+//   updateTarget();
+  
+// }else {
+//   miss++;
+//   missLabel.textContent = miss;
+// }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const words = ['apple', 'sky', 'blue'];
+// let word = words[Math.floor(Math.random() * words.length)];
+// let loc;
+// let score;
+// let miss;
+// let timeLimit =  3 * 1000;
+// let startTime;
+// let isPlaying = false;
+
+// const target = document.getElementById('target');
+// const scoreLabel = document.getElementById('score');
+// const missLabel = document.getElementById('miss');
+// const timerLabel = document.getElementById('timer');
+
+
+// function updateTarget() {
+// let placeholder = '';
+// for(let i = 0; i < loc; i++) {
+//   placeholder += '_';
+// }
+// target.textContent = placeholder + word.substring(loc);
+// }
+
+// function updateTimer() {
+//   const timeLeft = startTime + timeLimit -Date.now();
+//   timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+
+//   const timeOutId = setTimeout(() => {
+// updateTimer();
+//   }, 10);
+
+//   if(timeLeft < 0) {
+//     isPlaying = false;
+//     clearTimeout(timeOutId);
+//     timerLabel.textContent = '0.00';
+//     setTimeout(() => {
+//       showResult();
+//     }, 100);
+
+//     target.textContent = 'click to replay';
+//   }
+// }
+
+// function showResult(){
+//  const accuracy = score + miss === 0 ? 0 : score / (score * miss) * 100
+//  alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy!`);
+// }
+
+// window.addEventListener('click', () => {
+//   if(isPlaying === true){
+//     return;
+//   }
+//   isPlaying = true;
+
+//   loc = 0;
+//   score = 0;
+//    miss = 0;
+//    scoreLabel.textContent = score;
+//    missLabel.textContent = miss;
+//    word = words[Math.floor(Math.random() * words.length)];
+//   target.textContent = word;
+//   startTime = Date.now();
+//   updateTimer();
+// });
+
+// window.addEventListener('keydown', (e) => {
+//   if ( isPlaying !== true) {
+//     return;
+//   }
+// if(e.key === word[loc]) {
+//   loc++;
+//   if(loc === word.length) {
+//     word = words[Math.floor(Math.random() * words.length)];
+//     loc = 0;
+//   }
+//   updateTarget();
+//   score++;
+//   scoreLabel.textContent = score;
+// } else {
+//   miss++;
+//   missLabel.textContent = miss;
+// }
+// });
